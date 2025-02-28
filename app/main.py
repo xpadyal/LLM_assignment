3cbb4888302e2b9ef1ad7db5a770cb2a0e8665d4
xpadyal <padyal.s@northeastern.edu>
2025-02-28 16:13:51 -0500

    updated

--- app/main.py
+++ app/main.py
@@ -1,16 +1,26 @@
 from fastapi import FastAPI
 from config import TITLE, VERSION, DESCRIPTION, HOST, PORT
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
 from endpoints import wrapped_chain
 from langserve import add_routes
 from middleware import add_cors
 from notes import transcript, soap_note
 import uvicorn
 
+
 app = FastAPI(
     title=TITLE,
     version=VERSION,
     description=DESCRIPTION
 )
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/") 
+def serve_frontend():
+    return FileResponse("static/index.html")
+
 
 #Fetch the notes(trnascript, soap notes)
 add_routes(app, wrapped_chain, path="/ask")
